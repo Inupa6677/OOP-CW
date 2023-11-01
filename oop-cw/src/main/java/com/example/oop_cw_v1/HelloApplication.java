e7a8cff631a2b926807b928b17c803a021fd3ed0
Hasinindu Jayashan <137883302+HJayashan@users.noreply.github.com>
2023-11-01 15:00:15 +0530

    transitions updated

--- oop-cw/src/main/java/com/example/oop_cw_v1/HelloApplication.java
+++ oop-cw/src/main/java/com/example/oop_cw_v1/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("SACMS");
         stage.setScene(scene);
         stage.show();
     }
--- oop-cw/src/main/java/com/example/oop_cw_v1/HelloController.java
+++ oop-cw/src/main/java/com/example/oop_cw_v1/HelloController.java
@@ -6,8 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -15,14 +19,32 @@ public class HelloController {
 
     @FXML
     private Label label_registerNow;
-    // he knew
-    // 18 damma
+    @FXML
+    private Button btn_back;
+    @FXML
+    private Label label_error;
+    @FXML
+    private TextField txt_email;
+
 
     @FXML
-    private void switchToRegisterNow(ActionEvent actionEvent) throws IOException {
-        FXMLLoader registerPageLoader = new FXMLLoader(getClass().getResource("Register.fxml"));
-        Parent root = registerPageLoader.load();
+    private void switchToRegisterNow(MouseEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-
+        Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    @FXML
+    private void switchToLogin(ActionEvent event) throws IOException {
+        Stage stage = (Stage) btn_back.getScene().getWindow(); // Assuming backButton is the ID of your back button
+
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
