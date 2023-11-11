ad4bcaee0a2e1b524346580fd9dc2a36f1d522c5
Naveen-Savi08 <naveensavinda891@gmail.com>
2023-11-11 23:57:51 +0530

    Create Gui for Advisor and Create club

--- oop-cw/src/main/java/com/example/oopcw/HelloController.java
+++ oop-cw/src/main/java/com/example/oopcw/HelloController.java
@@ -1,22 +1,37 @@
 package com.example.oopcw;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Inupa Application!");
-    }
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the club Id: ");
-        int clubId = input.nextInt();
     }
+
+    @FXML
+    private Button btnCreate;
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Group groupFirst;
+
+    @FXML
+    private Group groupSecond;
+
+
+    public void onClickCreateClub(ActionEvent actionEvent) {
+        groupFirst.setVisible(false);
+        groupSecond.setVisible(true);
+
+    }
+
+
 }
