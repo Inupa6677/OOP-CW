a66d423658ff10b143d4f80a3ae869589dd3b3ca
Naveen-Savi08 <naveensavinda891@gmail.com>
2023-11-15 21:21:53 +0530

    Do validation

--- oop-cw/src/main/java/com/example/oopcw/HelloController.java
+++ oop-cw/src/main/java/com/example/oopcw/HelloController.java
@@ -4,10 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.sql.SQLException;
@@ -45,6 +47,8 @@ public class HelloController {
 
     @FXML
     private Group groupSecond;
+    @FXML
+    private ImageView importClubImageView;
 
 
     public void disableGroups() {
@@ -73,11 +77,56 @@ public class HelloController {
         String advisorId = txtAdvisorId.getText();
         String clubDescription = txtDescription.getText();
 
-        // do the validation part and after that add the data to database
-        Club club = new Club(clubID,clubName,members,advisorId,clubDescription);
-        DatabaseConnection.testDatabaseConnection();
-        DatabaseConnection.insertClubData(club);
-    }
+        boolean isValid = true;
 
+        // Validate clubID
+        if (!isValidNumber(clubID)) {
+            txtId.setStyle("-fx-text-fill: red;");
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Club ID");
+            alert.setHeaderText(null);
+            alert.setContentText("Enter only integer for Club ID.");
+            alert.showAndWait();
+            isValid = false;
+        }
+
+        // Validate members
+        if (!isValidNumber(members)) {
+            txtMembers.setStyle("-fx-text-fill: red;");
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Members");
+            alert.setHeaderText(null);
+            alert.setContentText("Enter only integers for Members.");
+            alert.showAndWait();
+            isValid = false;
+        }
+
+        // Validate advisorId
+        if (!isValidNumber(advisorId)) {
+            txtAdvisorId.setStyle("-fx-text-fill: red;");
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Advisor ID");
+            alert.setHeaderText(null);
+            alert.setContentText("Enter only integers for Advisor ID.");
+            alert.showAndWait();
+            isValid = false;
+        }
+
+        if (isValid) {
+            // Add the data to the database
+            Club club = new Club(clubID, clubName, members, advisorId, clubDescription);
+            DatabaseConnection.testDatabaseConnection();
+            DatabaseConnection.insertClubData(club);
+        }
+    }
+    private boolean isValidNumber(String input) {
+        try {
+            int parsedValue = Integer.parseInt(input);
+            return parsedValue >= 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
+
 
